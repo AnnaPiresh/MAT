70263f275080ecb11134d1a3b660fa4653615930
AnnaPiresh <a.piresh@qamadness.com>
2015-12-15 14:44:19 +0200

    commit

---
+++ src/test/java/com/qamadness/Story/PlaceOrderExistingStory.java
@@ -0,0 +1,14 @@
+package com.qamadness.Story;
+
+
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.junit.runners.ThucydidesRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ThucydidesRunner.class)
+public class PlaceOrderExistingStory {
+
+    @Managed(uniqueSession = true)
+
+
+}
---
+++ src/test/java/com/qamadness/steps/PlaceOrderExistingSteps.java
@@ -0,0 +1,9 @@
+package com.qamadness.steps;
+
+
+import net.thucydides.core.steps.ScenarioSteps;
+
+public class PlaceOrderExistingSteps extends ScenarioSteps{
+
+
+}
