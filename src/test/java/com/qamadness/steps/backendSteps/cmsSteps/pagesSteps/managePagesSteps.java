4d9fdb9ac0d5adc0852c7495b37af7d58641bb2b
AnnaPiresh <a.piresh@qamadness.com>
2016-01-26 18:48:15 +0200

    Finished "Delete Attribute Set" story, added methods and objects to Products pages and Steps

--- src/test/java/com/qamadness/steps/backendSteps/cmsSteps/pagesSteps/managePagesSteps.java
+++ src/test/java/com/qamadness/steps/backendSteps/cmsSteps/pagesSteps/managePagesSteps.java
@@ -1,39 +1,40 @@
 package com.qamadness.steps.backendSteps.cmsSteps.pagesSteps;
 
+import com.qamadness.pages.backend.cms.pages.ManagePagesPage;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 
 /**
  * Created by Alexandra on 1/25/16.
  */
-public class managePagesSteps extends ScenarioSteps{
+public class ManagePagesSteps extends ScenarioSteps{
 
-    com.qamadness.pages.backend.cms.pages.managePagesPage managePagesPage;
+    ManagePagesPage ManagePagesPage;
 
     //Click General buttons steps:
 
     @Step
     public void click_Add_New_Page_Button (){
-        managePagesPage.clickAddNewPageButton();
+        ManagePagesPage.clickAddNewPageButton();
     }
 
     //Methods for grid:
 
     @Step
     public void filter_Pages_By_Url_Key (String urlKey){
-        managePagesPage.filterPagesByUrlKey(urlKey);
+        ManagePagesPage.filterPagesByUrlKey(urlKey);
     }
 
     @Step
     public void click_First_Page_In_The_Grid (){
-        managePagesPage.clickFirstPageInTheGrid();
+        ManagePagesPage.clickFirstPageInTheGrid();
     }
 
     //Verifications:
 
     @Step
     public void verify_That_Success_Saved_Page_Message_Is_Displayed(){
-        managePagesPage.verifyThatSuccessSavedPageMessageIsDisplayed();
+        ManagePagesPage.verifyThatSuccessSavedPageMessageIsDisplayed();
     }
 
 
