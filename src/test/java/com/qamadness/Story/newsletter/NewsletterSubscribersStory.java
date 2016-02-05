d1f801feb36f941b4e2b03bdae8531ea844d2164
Vladislav <v.ershov@qamadness.com>
2016-02-05 15:21:35 +0200

    [Created]MAT-234  Newsletter: Subscribe registered customer email

--- src/test/java/com/qamadness/Story/newsletter/NewsletterSubscribersStory.java
+++ src/test/java/com/qamadness/Story/newsletter/NewsletterSubscribersStory.java
@@ -175,6 +175,38 @@ public class NewsletterSubscribersStory {
         homePageSteps.check_Subscribe_Error_Msg();
         webDriver.close();
     }
+
+    @Issue("MAT-234")
+    @Pending
+    @Test
+    public void subscribe_registered_customer_email() throws AWTException {
+        loginPageSteps.openPage();
+        loginPageSteps.loginInput(login);
+        loginPageSteps.passInput(password);
+        loginPageSteps.loginButton();
+        dashboardSteps.closePopup();
+        homePageSteps.open_Home_Page();
+        homePageSteps.open_account_menu_in_header();
+        homePageSteps.click_register_link();
+        registrationPageSteps.enter_First_Name(firstName);
+        registrationPageSteps.enter_Last_Name(lastName);
+        registrationPageSteps.enter_Email_Address(email);
+        registrationPageSteps.enter_Password(passwordRegister);
+        registrationPageSteps.enter_Password_To_Confirm_Fld(passwordRegister);
+        registrationPageSteps.click_Sign_Up_Newsletter_Checkbox();
+        registrationPageSteps.click_Register_Btn();
+        registrationPageSteps.check_Success_Registration_Msg();
+        homePageSteps.open_Home_Page();
+        homePageSteps.enter_Email_To_Newsletter_Field(email);
+        homePageSteps.click_Subscribe_Btn();
+        homePageSteps.check_Success_Subscribe_Msg();
+        loginPageSteps.navigate_to_dashboard();
+        mainMenuSteps.open_Manage_Customers_Page();
+        manageCustomersSteps.search_Customer_By_Email(email);
+        manageCustomersSteps.open_First_Customer_Profile();
+        customerInformationSteps.click_Delete_Customer_Button();
+        logoutFromAdminSteps.logout_from_admin();
+    }
 }
 
 
